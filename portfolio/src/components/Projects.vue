bfc357111be5da3a70244d1a9ee8a7a6de9ace5e
Scott Norton <scott.norton12@gmail.com>
2020-09-13 18:38:54 -0400

    added slippistatsonline to projects

--- portfolio/src/components/Projects.vue
+++ portfolio/src/components/Projects.vue
@@ -2,22 +2,25 @@
   <v-container id="projectsContainer" fluid>
     <div class="projectsTitle">Projects</div>
     <v-container class="gridContianer">
-      <v-row justify="center">
-        <v-card
-        v-for="(item, i) in projects"
-        :key="i"
-        ripple 
-        class="projectCard" 
-        :href="item.link" 
-        target="_blank">
-          <v-img
-            height="200px"
-            width="200px"
-            :src="getImg(item.img)"
+      <v-row justify="center" no-gutters>
+        <v-col cols="7" md="3" v-for="(item, i) in projects" :key="i">
+          <v-card
+            ripple 
+            class="projectCard" 
+            :href="item.link" 
+            target="_blank"
+            width="220px"
+            height="375px"
           >
-          </v-img>
-          <v-card-title class="justify-center">{{ item.title }}</v-card-title>
-        </v-card>
+            <v-img
+              height="200px"
+              :src="getImg(item.img)"
+            >
+            </v-img>
+            <v-card-title class="justify-center">{{ item.title }}</v-card-title>
+            <v-card-text class="description">{{ item.description }}</v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
       <div style="text-align: center;">(This website was also made from <a href="https://github.com/scottn12/Portfolio" target="_blank">scratch</a>!)</div>
     </v-container>
@@ -33,20 +36,29 @@ export default {
   data() {
     return {
       projects: [
+        {      
+          title: 'Slippi Stats Online',
+          img: 'Slippi.png',
+          link: 'https://github.com/scottn12/ScottBot',
+          description: 'Website designed to provide a way to analyze a large number of Slippi Online games at once.'
+        },
         {
           title: 'ScottBot',
           img: 'ScottBot.jpg',
-          link: 'https://github.com/scottn12/ScottBot'
+          link: 'https://github.com/scottn12/ScottBot',
+          description: 'Dicsord Bot that provides quality of life commands, mini-games, and more.'
         },
         {
           title: 'MagicMirror',
           img: 'MagicMirror.jpeg',
-          link: 'https://github.com/scottn12/MagicMirror'
+          link: 'https://github.com/scottn12/MagicMirror',
+          description: 'Code designed for a smart mirror display.'
         },
-                {
-          title: 'AttentionButton',
+        {
+          title: 'Attention Button',
           img: 'AttentionButton.jpg',
-          link: 'https://github.com/scottn12/AttentionButton'
+          link: 'https://github.com/scottn12/AttentionButton',
+          description: 'Android application used to remotely notify if attention is needed.'
         },
       ]
     }
@@ -70,8 +82,11 @@ export default {
 }
 
 .projectCard {
-  margin-left:15px;
-  margin-right: 15px;
   margin-bottom: 15px;
+}
+
+.description {
+  max-width: 220px;
+  text-align: center;
 }
 </style>
